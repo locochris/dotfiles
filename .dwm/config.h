0af13b9db76a734c93339fe9404355daca1448ed
Vasily Mikhaylichenko <vasily.mikhaylichenko@gmail.com>
2012-07-15 21:23:51 +1000

    Add network status indicator to dwm; modify AC / battery icon

--- .dwm/config.h
+++ .dwm/config.h
@@ -17,7 +17,7 @@ static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "term", "web", "im", "files"};
+static const char *tags[] = { "term", "web", "text", "msg", "files"};
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
